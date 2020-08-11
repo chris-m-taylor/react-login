ac82994fbc692276923e06089f9785b2081449d9
Chris Taylor <chrismtaylor1998@gmail.com>
2020-08-11 16:21:02 -0400

    users are now contained in an array of objects

---
+++ src/users.js
@@ -0,0 +1,22 @@
+export const users = [
+    {
+        username: 'Jeff123',
+        password: 'supersecret'
+
+    },
+    {
+        username: 'JoshIsCool',
+        password: 'passwordIsCool'
+
+    },
+    {
+        username: 'ChrisIsCooler',
+        password: 'passwordIsCooler'
+
+    },
+    {
+        username: 'ZebraFan',
+        password: 'secretzebras'
+
+    }
+]
